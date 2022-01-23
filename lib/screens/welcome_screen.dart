ee11839a0c9834b66e866a2b6d35f09ed9689f91
udit2812 <udit2812@gmail.com>
2022-01-23 16:34:34 +0530

    Flash Chat

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,15 +1,42 @@
+import 'package:flash_chat/components/rounded_button.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'login_screen.dart';
+// import 'package:flash_chat/components/rounded_button';
 
 class WelcomeScreen extends StatefulWidget {
+  static String id='welcome_screen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+
+  AnimationController controller;
+  Animation animation;
+  @override
+  void initState() {
+    super.initState();
+    controller=AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this);
+
+      animation= ColorTween(begin: Colors.blueGrey, end: Colors.white).animate(controller);
+      
+          controller.forward();
+    controller.addListener(() {
+      setState(() {
+        
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.white.withOpacity(controller.value),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -18,61 +45,43 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
-                ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                 Hero(
+              tag:'logo',
+              child: Container(
+                height: 60.0,
+                child: Image.asset('images/logo.png'),
+              ),
+            ),
+                AnimatedTextKit(
+                  animatedTexts: [TypewriterAnimatedText('Flash Chat',
+                  textStyle:TextStyle(
+                    color: Colors.black87,
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
                   ),
+                  )],
                 ),
               ],
             ),
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
+            RoundedButton(
+              title: 'Register',
+              colour: Colors.blueAccent,
+              onPressed: (){Navigator.pushNamed(context, RegistrationScreen.id);
+              },
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
+             RoundedButton(
+              title: 'Login',
+              colour: Colors.lightBlueAccent,
+              onPressed: (){Navigator.pushNamed(context, LoginScreen.id);
+              },
               ),
-            ),
           ],
         ),
       ),
     );
   }
 }
+
